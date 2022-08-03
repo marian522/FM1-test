b43fb670d1f43a8dd1b22f3f2cfd78866cbf104f
Marian <marian.ahu.nob@gmail.com>
2022-08-03 19:07:12 -0300

    Resultado

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,12 +2,24 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-var binario = 0
+var array = num.split("");
+var sum = 0;
+for (var i=0; i<array.length; i++){
+  sum += array[i] * 2 ** (array.length -1 -i);
+}
+return sum; 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+if (num <= 0) return "0";
+var array = [];
 
+while (num > 0){
+  array.unshift(num%2);
+  num = Math.floor(num/2);
+}
+return array.join ("");
 }
 
 
