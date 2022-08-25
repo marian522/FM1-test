8b92cd7996e1e5e7e95033f9873749102404c66b
Marian <marian.ahu.nob@gmail.com>
2022-08-24 22:27:42 -0300

    HW7

--- FT-M1-master/07-Algoritmos-I/homework/homework.js
+++ FT-M1-master/07-Algoritmos-I/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
 
 function factorear(num) {
@@ -7,6 +7,17 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  var array = [1]; // [1,2,2,3,3,5] // nos mandan el 180 por argumento.
+  var i = 2;
+  while (num > 1) {
+    if (num % i === 0) {
+      array.push(i);
+      num = num / i;
+    } else {
+      i++;
+    }
+  }
+  return array;
 }
 
 function bubbleSort(array) {
@@ -14,27 +25,44 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  var cambio = true;
+while(cambio){
+  cambio = false;
+  for(var i = 0; i < array.length - 1; i++){
+    if(array[i] > array[i + 1]){
+      let aux = array[i];
+      array[i] = array[i + 1];
+      array[i + 1] = aux;
+      cambio = true;
+    }
+  }
 }
-
+return array;
+}
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (var i = 1; i < array.length; i++) {
+    var j = i - 1;
+    var aux = array[i];
+    while (j >= 0 && array[j] > aux) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = aux;
+  }
+  return array;
 }
-
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
 }
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
